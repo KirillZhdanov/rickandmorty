6e865dcd8b212327c9eb2faa0eb16f0689739e85
KirillZh <jd_senar@mail.ru>
2021-02-12 13:28:18 +0300

    feat:loader

--- src/pages/AdditionalInfoPage.tsx
+++ src/pages/AdditionalInfoPage.tsx
@@ -24,29 +24,31 @@ type TSelected = {
 const AdditionalInfoPage: React.FC<Match> = ({ match }) => {
     const character = useSelector((state: RootState | TSelected) => state.characterByIDReducer);
     const dispatch = useDispatch();
-    const [visible, setVisible] = React.useState(false);
+
+    const visible = (character.name.length > 0);
     React.useEffect(() => {
         const characterId = Number.parseInt(match.url.substring(1));
         dispatch(fetchById(characterId));
-        setTimeout(() => {
-            setVisible(true);
-        }, 500);
     }, [])
 
     return (
-        visible ? (<div>
-            <StyledLink as={Link} to="/"><ArrowBack size={18} /> Back</StyledLink>
+        visible ? (
             <div>
-                <StyledImage className="avatar" src={character.image} alt={`${character.name} image`} />
-            </div>
-            <StyledSubTitle>{character.name}</StyledSubTitle>
-            <div>
-                <p>Status: {character.status}</p>
-                <p>Gender: {character.gender}</p>
-                <p>Species: {character.species}</p>
-                <p>Location: {character.location?.name}</p>
-            </div>
-        </div>) : <div className="lds-dual-ring" style={{ position: "absolute", top: "50%", }}></div>
+                <StyledLink as={Link} to="/"><ArrowBack size={18} /> Back</StyledLink>
+                <div>
+                    <StyledImage className="avatar" src={character.image} alt={`${character.name} image`} />
+                </div>
+                <StyledSubTitle>{character.name}</StyledSubTitle>
+                <div>
+                    <p>Status: {character.status}</p>
+                    <p>Gender: {character.gender}</p>
+                    <p>Species: {character.species}</p>
+                    <p>Location: {character.location?.name}</p>
+                </div>
+            </div>) : (<>
+                <StyledLink as={Link} to="/"><ArrowBack size={18} /> Back</StyledLink>
+                <div className="lds-dual-ring" style={{ position: "absolute", top: "50%", left: "50%" }}></div>
+            </>)
     )
 }
 
